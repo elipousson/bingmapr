004f228d12678b4512cdc3aae89006c3e84c90b7
Eli Pousson <eli.pousson@gmail.com>
2021-05-13 22:38:00 -0400

    Add get_map_meta function + nudge parameter
    - get_map_meta function optionally returns a bbox from the metadata
    - Update README to show nudge parameter
    - Add check parameter to optionally return error if get_request_url generates a bad URL

--- R/map_image.R
+++ R/map_image.R
@@ -17,10 +17,13 @@
 #' - CanvasDark: A dark version of the road maps.
 #' - CanvasLight: A lighter version of the road maps which also has some of the details such as hill shading disabled.
 #' - CanvasGray: A grayscale version of the road maps
-#' @param mapsize Numeric vector with width and height in pixels, Default: c(600, 400) 600px wide, 400px high
+#' @param mapsize Numeric vector with width and height in pixels, Default: c(600, 400) or 600px wide, 400px high
 #' @param zoom Numeric vector between 0 and 20 for imagery other than Bird's Eye maps or 18 to 22 for Bird's Eye maps. Default: 18
 #' @param orientation Character string with orientation  ("N", "E", "S", "W") or numeric direction (0,90,180,270), Default: 0
+#' @param nudge Numeric vector in the format, c(meters to shift latitude, meters to shift longitude), e.g. c(100, 0) to shift center 100 meters in latitude
+#' @param bbox If TRUE, return a bbox class object based on the bounding box values from the map metadata. If FALSE (default), return the full JSON metadata. (get_map_meta only)
 #' @param key Bing Maps API Key, Default: Sys.getenv("BING_MAPS_API_KEY")
+#' @param check If TRUE, check the map metadata which returns an error if the image is unavailable (Default FALSE for get_request_url and TRUE for get_map_image)
 #' @name bing_static_map
 #' @md
 NULL
@@ -29,13 +32,16 @@ NULL
 #' @rdname bing_static_map
 #' @importFrom sf st_centroid st_transform st_coordinates
 #' @importFrom utils URLencode
+#' @importFrom jsonlite read_json
 get_request_url <- function(location = NULL,
                             query = NULL,
                             imagery = "BirdsEye",
                             mapsize = c(600, 400),
                             zoom = 18,
                             orientation = 0,
-                            key = Sys.getenv("BING_MAPS_API_KEY")) {
+                            nudge = NULL,
+                            key = Sys.getenv("BING_MAPS_API_KEY"),
+                            check = FALSE) {
   imagery_options <- c("Aerial", "AerialWithLabels", "AerialWithLabelsOnDemand", "Streetside", "BirdsEye", "BirdsEyeWithLabels", "Road", "CanvasDark", "CanvasLight", "CanvasGray")
   imagery <- match.arg(imagery, imagery_options)
 
@@ -49,6 +55,21 @@ get_request_url <- function(location = NULL,
     location <- suppressWarnings(sf::st_centroid(location))
     location <- sf::st_coordinates(sf::st_transform(location, 4326))
     location <- rev(c(location))
+  }
+
+  if (is.numeric(nudge)) {
+    nudge_lat <- nudge[1]
+    nudge_lon <- nudge[2]
+
+    lat <- location[1]
+    lon <- location[2]
+
+    # from https://stackoverflow.com/questions/7477003/calculating-new-longitude-latitude-from-old-n-meters
+    # 6371000.0 is approximate radius of the earth in meters
+    new_lat <- lat + (nudge_lat / 6371000.0) * (180 / pi)
+    new_lon <- lon + (nudge_lon / 6371000.0) * (180 / pi) / cos(lat * pi / 180)
+
+    location <- c(new_lat, new_lon)
   }
 
   location <- paste(location, collapse = ",")
@@ -85,6 +106,10 @@ get_request_url <- function(location = NULL,
   path <-
     paste0(path, "?", query_string)
 
+  if (check) {
+    meta <- jsonlite::read_json(paste0(path, "&mapMetadata=1"))
+  }
+
   return(path)
 }
 
@@ -98,7 +123,10 @@ get_map_image <- function(location = NULL,
                           mapsize = c(600, 400),
                           zoom = 18,
                           orientation = 0,
-                          key = Sys.getenv("BING_MAPS_API_KEY")) {
+                          nudge = NULL,
+                          key = Sys.getenv("BING_MAPS_API_KEY"),
+                          check = TRUE) {
+
   path <- get_request_url(
     location,
     query,
@@ -106,12 +134,58 @@ get_map_image <- function(location = NULL,
     mapsize,
     zoom,
     orientation,
-    key
+    nudge,
+    key,
+    check
   )
 
   map <- jpeg::readJPEG(RCurl::getURLContent(path))
 
   return(map)
+}
+
+
+#' @return get_map_meta returns the JSON with the map metadata or a bbox for the map area
+#' @rdname bing_static_map
+#' @importFrom jsonlite read_json
+#' @importFrom sf st_bbox st_crs
+get_map_meta <- function(location = NULL,
+                         query = NULL,
+                         imagery = "BirdsEye",
+                         mapsize = c(600, 400),
+                         zoom = 18,
+                         orientation = 0,
+                         nudge = NULL,
+                         key = Sys.getenv("BING_MAPS_API_KEY"),
+                         bbox = FALSE) {
+  path <- get_request_url(
+    location,
+    query,
+    imagery,
+    mapsize,
+    zoom,
+    orientation,
+    nudge,
+    key
+  )
+
+  meta <- jsonlite::read_json(paste0(path, "&mapMetadata=1"))
+
+  if (bbox) {
+    bbox <- sf::st_bbox(
+      c(
+        ymin = meta$resourceSets[[1]]$resources[[1]]$bbox[[1]],
+        xmin = meta$resourceSets[[1]]$resources[[1]]$bbox[[2]],
+        ymax = meta$resourceSets[[1]]$resources[[1]]$bbox[[3]],
+        xmax = meta$resourceSets[[1]]$resources[[1]]$bbox[[4]]
+      ),
+      crs = sf::st_crs(4326)
+    )
+
+    return(bbox)
+  } else {
+    return(meta)
+  }
 }
 
 #' @return plot_map plots the static map image
@@ -123,6 +197,7 @@ plot_map_image <- function(location = NULL,
                            mapsize = c(600, 400),
                            zoom = 18,
                            orientation = 0,
+                           nudge = NULL,
                            key = Sys.getenv("BING_MAPS_API_KEY")) {
   map <- get_map_image(
     location,
@@ -131,11 +206,13 @@ plot_map_image <- function(location = NULL,
     mapsize,
     zoom,
     orientation,
-    key
+    nudge,
+    key,
+    check = TRUE
   )
 
   # from https://stackoverflow.com/questions/9543343/plot-a-jpg-image-using-base-graphics-in-r#28729601
   map_res <- dim(map)[2:1] # get the resolution, [x, y]
-  plot(1, 1, xlim = c(1, map_res[1]), ylim = c(1, map_res[2]), asp = 1, type = "n", xaxs = "i", yaxs = "i", xaxt = "n", yaxt = "n", xlab = "", ylab = "", bty = "n")
-  graphics::rasterImage(map, 1, 1, map_res[1], map_res[2])
+  plot(0, 0, xlim = c(0, map_res[1]), ylim = c(0, map_res[2]), asp = 1, type = "n", xaxs = "i", yaxs = "i", xaxt = "n", yaxt = "n", xlab = "", ylab = "", bty = "n")
+  graphics::rasterImage(map, 0, 0, map_res[1], map_res[2])
 }
--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 <!-- badges: start -->
 <!-- badges: end -->
 
-bingmapr is an R packge for retrieving and plotting maps from the [Bing Static Maps API](). I created this package primarily to provide a convenient way to access Bing's Bird's Eye imagery and not all API parameters are currently supported.
+bingmapr is an R packge for retrieving and plotting maps from the [Bing Static Maps API](https://docs.microsoft.com/en-us/bingmaps/rest-services/imagery/get-a-static-map). As of May 2021, I created and plan to use package mainly as a convenient way to access Bing's Bird's Eye (oblique-angle) imagery so the routes and traffic features of the API are all not currently supported.
 
 This package is inspired by the [snapbox package](https://github.com/anthonynorth/snapbox/) that allows the use of the maps from the Mapbox static map API in ggplot maps. I hope to integrate bingmapr with ggplot in the future.
 
@@ -24,11 +24,19 @@ remotes::install_github("elipousson/bingmapr")
 ``` r
 library(bingmapr)
 
+# Get a Bing Maps API Key https://www.bingmapsportal.com/
+bing_maps_api_key(key = "put your key here", install = TRUE)
+
 # Get request URL centered on Washington Square Park in New York, NY
 get_request_url(location = c(40.7308349, -73.99746074), orientation = "W")
 
-# Plot a static map showing the center of Wake County, North Carolina
+# Get an sf object to show how bingmapr can use sf objects for locations
 nc <- sf::st_read(system.file("shape/nc.shp", package="sf"))
+
+# Plot a static map showing the center of Wake County, North Carolina
 plot_map_image(location = nc[nc$NAME == "Wake",], orientation = 180)
+
+# Plot static map centered 300 meters north and 500 meters west of the center
+plot_map_image(location = nc[nc$NAME == "Wake",], orientation = 180, nudge = c(300, 500))
 ```
 
